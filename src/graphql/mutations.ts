6051aac4af6a7979b166e8e0f1c3e7234119666a
Adam Naoui <adam.naoui@polymtl.ca>
2022-12-05 11:43:23 -0500

    hotfix for verified email page

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -28,15 +28,23 @@ export const SYNC_WOOCOMMERCE = gql`
 `
 export const VERIFY_EMAIL = gql`
     mutation Mutation($token: String!) {
-        verifyVendorAccount(token: $token) {
-            code
-            message
-            vendorAccount {
-                _id
-                store {
+        verifyAccount(token: $token) {
+            __typename
+            ... on ClientAccountResponse {
+                code
+                message
+                clientAccount {
                     _id
                 }
             }
+            ... on VendorAccountResponse {
+                code
+                message
+                vendorAccount {
+                    _id
+                }
+            }
+            
         }
     }
 `
--- src/pages/email_verified/EmailVerified.tsx
+++ src/pages/email_verified/EmailVerified.tsx
@@ -11,41 +11,43 @@ import {EMAIL_VERIFIED_LOGIN_KEY, EMAIL_VERIFIED_MESSAGE_KEY} from "../../transl
 const EmailVerified = () => {
   const params = useParams()
   const [isTokenInactive, setTokenAsInactive] = useState(false)
-  const handleVerifyToken = (verifyData: any) => {
-    const tokenVerified = verifyData.verifyVendorAccount.code === 200
+
+  const [verifyEmail, {data, loading, error}] = useMutation(VERIFY_EMAIL);
+
+  useEffect(() => {
+    console.log(params.token)
+    verifyEmail({variables: {token: params.token}}).then(r => {
+      console.log(r)
+    })
+  }, [params.token, verifyEmail])
+
+  useEffect(() => {
+    const tokenVerified = data.verifyAccount.code === 200
     setTokenAsInactive(!tokenVerified)
-  }
+  }, [data])
 
-    const [verifyEmail, {loading, error}] = useMutation(VERIFY_EMAIL, {onCompleted: handleVerifyToken});
+  const classes = emailVerifiedStyles()
+  const {t: translation} = useTranslation()
+  const navigate = useNavigate()
 
-    useEffect(() => {
-      console.log(params.token)
-      verifyEmail({variables: {token: params.token}}).then(r => {
-        console.log(r)
-      })
-    }, [])
-
-    const classes = emailVerifiedStyles()
-    const {t: translation} = useTranslation()
-    const navigate = useNavigate()
-
-    if (loading) return <p>Loading...</p>;
-    if (error || isTokenInactive) return <p>Error :(</p>; //TODO Change isTokenInactive
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (isTokenInactive) return <p>Token is inactive</p>;
 
 
-    return (
-      <Grid container direction="column" className={classes.root}>
-        <Grid container direction="column" xs={2} className={classes.form}>
-          <img style={{margin: 10}} src={green_check} height={"200px"} width={"200px"} alt={"checkbox"}/>
-          <Typography>
-            {translation(EMAIL_VERIFIED_MESSAGE_KEY)}
-          </Typography>
-          <Link style={{margin: 10}} className={classes.link} onClick={() => navigate("/login")}>
-            {translation(EMAIL_VERIFIED_LOGIN_KEY)}
-          </Link>
-        </Grid>
+  return (
+    <Grid container direction="column" className={classes.root}>
+      <Grid container direction="column" xs={2} className={classes.form}>
+        <img style={{margin: 10}} src={green_check} height={"200px"} width={"200px"} alt={"checkbox"}/>
+        <Typography>
+          {translation(EMAIL_VERIFIED_MESSAGE_KEY)}
+        </Typography>
+        {(data.verifyAccount.vendorAccount) && <Link style={{margin: 10}} className={classes.link} onClick={() => navigate("/login")}>
+          {translation(EMAIL_VERIFIED_LOGIN_KEY)}
+        </Link>}
       </Grid>
-    )
-  }
+    </Grid>
+  )
+}
 
-  export default EmailVerified
+export default EmailVerified
